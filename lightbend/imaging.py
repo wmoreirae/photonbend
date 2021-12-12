83ac76f076e5a3716a6b78f822b1598b5eb75c68
Edson Moreira <w.moreirae@gmail.com>
2021-12-12 15:58:34 -0300

    Added the pixel level supersampling. No more need to resample the whole image

--- lightbend/imaging.py
+++ lightbend/imaging.py
@@ -2,14 +2,18 @@ import numpy as np
 from PIL import Image
 from numba import njit, prange
 
-
 from lightbend.utils import vector_magnitude, radians_to_degrees, degrees_to_radians, calculate_pixels_per_f_distance, \
     dpi_to_dpmm, \
     unit_vector, vector_to_focal_units, c_round
 
-
+# TODO
+"""
+Split this function into a function that calculates the image sizes, focal distances and pixels por focal distances and
+the one that process the pixel data
+"""
 @njit(parallel=True)
-def process_image(o_image_arr, lens_angle, function_o, ifunction_o, function_d, ifunction_d, fullframe):
+def process_image(src_image_arr, lens_angle, function_source, ifunction_source, function_destiny, ifunction_destiny,
+                  fullframe):
     """
     Firstly, the program reads the position from the destination, calculates the theta angle of the destination
     and then it translates it to a position on the origin, reads such position and writes it on the position
@@ -17,56 +21,85 @@ def process_image(o_image_arr, lens_angle, function_o, ifunction_o, function_d, 
     """
 
     # Some defaults
-    f_distance = 1.0
-    y_size_o, x_size_o = o_image_arr.shape[:2]
+    focal_distance_src = 1.0
+    height_source, width_source = src_image_arr.shape[:2]
 
-    center_o = complex(x_size_o, y_size_o) / 2 - complex(0.5, 0.5)
+    center_source = complex(width_source, height_source) / 2 - complex(0.5, 0.5)
 
-    # compute the pixels per focal distance
-    pixels_pfd_origin = calculate_pixels_per_f_distance(center_o, lens_angle, f_distance, function_o)
+    # compute the pixels per focal distance of the source image
+    pixels_fd_source = calculate_pixels_per_f_distance(center_source, lens_angle, focal_distance_src, function_source)
 
     # Calculate the max angle of the lens
     if fullframe:
-        max_vector_o = center_o
+        max_vector_o = center_source
     else:
-        max_vector_o = complex(center_o.real, 0)
+        max_vector_o = complex(center_source.real, 0)
 
-    # Compute the focal distance of the destiny lens so it can have the same angle as the origin lens
-    f_factor = vector_magnitude(max_vector_o) / (function_d(lens_angle / 2) * pixels_pfd_origin)
-    f_distance_d = f_distance * f_factor
+    focal_distance_destiny = compute_f_distance_destiny(focal_distance_src, function_destiny, lens_angle, max_vector_o,
+                                                     pixels_fd_source)
 
-    # Compute the amount of pixels needed to not loose quality over different projections
-    pixels_degree_o = function_o(degrees_to_radians(1)) * f_distance
-    pixels_degree_d = function_d(degrees_to_radians(1)) * f_distance_d
-    quality_factor = pixels_degree_o / pixels_degree_d
-    ss_factor = 1  # TODO remove the hard coding of the super-sampling
-    quality_factor *= ss_factor
-    pixels_pfd_destiny = pixels_pfd_origin * quality_factor
+    quality_factor = compute_quality_factor(focal_distance_src, focal_distance_destiny, function_source, function_destiny)
+    quality_factor = quality_factor
+    pixels_fd_destiny = pixels_fd_source * quality_factor
 
     # Create the destiny array
-    x_size_d = int(np.round(x_size_o * quality_factor))
-    y_size_d = int(np.round(y_size_o * quality_factor))
+    x_size_d = int(np.round(width_source * quality_factor))
+    y_size_d = int(np.round(height_source * quality_factor))
 
-    center_d = complex(x_size_d, y_size_d) / 2 - complex(0.5, 0.5)
+    center_destiny = complex(x_size_d, y_size_d) / 2 - complex(0.5, 0.5)
     destiny_array = np.zeros((y_size_d, x_size_d, 3), 'uint8')
 
+    ss_factor = 3
+    center_ss_pixel = complex((ss_factor - 1) / 2, (ss_factor - 1) / 2)
+    ss_subpixel_distance = 1 / ss_factor
+
     for row in prange(y_size_d):
-        for column in range(x_size_d):
+        for column in prange(x_size_d):
 
-            position_d = complex(column, row)
-            position_vector = position_d - center_d
-            unit_position_vector = unit_vector(position_vector)
-            projection_magnitude = vector_to_focal_units(position_vector, f_distance_d, pixels_pfd_destiny)
+            # SUPER SAMPLING ROUNDS
+            ss_matrix = np.zeros((ss_factor, ss_factor, 3), 'uint8')
+            for ss_row in prange(ss_factor):
+                for ss_column in prange(ss_factor):
+                    ss_position = (complex(ss_column, ss_row) - center_ss_pixel) * ss_subpixel_distance
 
-            theta = ifunction_d(projection_magnitude)
+                    position_destiny = complex(column, row) + ss_position
+                    position_vector = position_destiny - center_destiny
+                    unit_position_vector = unit_vector(position_vector)
+                    projection_magnitude = vector_to_focal_units(position_vector, focal_distance_destiny, pixels_fd_destiny)
 
-            projection_magnitude_o = function_o(theta) * f_distance * pixels_pfd_origin
-            position_o = center_o + (unit_position_vector * projection_magnitude_o)
+                    theta = ifunction_destiny(projection_magnitude)
 
-            column_o, row_o = c_round(position_o)
+                    projection_magnitude_o = function_source(theta) * focal_distance_src * pixels_fd_source
+                    position_o = center_source + (unit_position_vector * projection_magnitude_o)
 
-            if 0 <= column_o < x_size_o and 0 <= row_o < y_size_o:
-                destiny_array[row, column, :] = o_image_arr[row_o, column_o, :]
+                    column_o, row_o = c_round(position_o)
+
+                    if 0 <= column_o < width_source and 0 <= row_o < height_source:
+                        ss_matrix[ss_row, ss_column, :] = src_image_arr[row_o, column_o, :]
+
+            # if 0 <= column_o < width_source and 0 <= row_o < height_source:
+            ss_mean_1st = np.mean(ss_matrix[:, :, 0])
+            ss_mean_2nd = np.mean(ss_matrix[:, :, 1])
+            ss_mean_3rd = np.mean(ss_matrix[:, :, 2])
+            destiny_array[row, column, 0] = int(np.round(ss_mean_1st))
+            destiny_array[row, column, 1] = int(np.round(ss_mean_2nd))
+            destiny_array[row, column, 2] = int(np.round(ss_mean_3rd))
 
     return destiny_array
 
+
+@njit
+def compute_f_distance_destiny(focal_distance_source, function_destiny, lens_angle, max_vector_o, pixels_fd_source):
+    """Compute the focal distance of the destiny lens so it can have the same angle as the origin lens"""
+    f_factor = vector_magnitude(max_vector_o) / (function_destiny(lens_angle / 2) * pixels_fd_source)
+    f_distance_d = focal_distance_source * f_factor
+    return f_distance_d
+
+
+@njit
+def compute_quality_factor(focal_distance_source, focal_distance_destiny, function_origin, function_destiny):
+    """ Compute scaling of the image needed not to loose quality over different mapping functions """
+    pixels_degree_o = function_origin(degrees_to_radians(1)) * focal_distance_source
+    pixels_degree_d = function_destiny(degrees_to_radians(1)) * focal_distance_destiny
+    quality_factor = pixels_degree_o / pixels_degree_d
+    return quality_factor
