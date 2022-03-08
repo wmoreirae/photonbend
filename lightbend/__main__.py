f796681aebf0e2a6522b98a1cb6b03b3e9db631a
Edson Moreira <w.moreirae@gmail.com>
2022-03-08 14:57:50 -0300

    Updated pipfile

--- lightbend/__main__.py
+++ lightbend/__main__.py
@@ -10,23 +10,4 @@ from lightbend.utils import degrees_to_radians
 from lightbend.core.sphere_image import SphereImage, ImageType
 
 if __name__ == '__main__':
-    # origin_image = Image.open('./images/fisheye_180.jpg')
-    # origin_arr = np.asarray(origin_image)
-    # s_origin = SphereImageInscribed(origin_arr, ImageType.INSCRIBED, np.pi, equisolid, equisolid_inverse)
-    # s_destiny = SphereImageInscribed(np.zeros([4096, 4096, 3], np.core.uint8), ImageType.INSCRIBED, degrees_to_radians(110),
-    #                                  rectilinear, rectilinear_inverse)
-    # s_destiny.map_from_sphere_image(s_origin)
-    # destiny_arr = s_destiny.get_image_array()
-    # destiny_image = Image.fromarray(destiny_arr)
-    # destiny_image.save('results/Rectilinear.jpg')
 
-    origin_image = Image.open('./images/fisheye_180.jpg')
-    origin_arr = np.asarray(origin_image)
-    s_origin = SphereImage(origin_arr, ImageType.INSCRIBED, degrees_to_radians(180), equisolid,
-                           equisolid_inverse)
-    s_destiny = SphereImage(np.zeros([1800, 2880, 3], np.core.uint8), ImageType.CROPPED_CIRCLE,
-                            degrees_to_radians(180), equidistant, equidistant_inverse)
-    s_destiny.map_from_sphere_image(s_origin)
-    destiny_arr = s_destiny.get_image_array()
-    destiny_image = Image.fromarray(destiny_arr)
-    destiny_image.save('results/Equidistant.jpg')
